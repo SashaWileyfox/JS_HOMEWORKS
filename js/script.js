e58f6d800e90b6e6612b3cbe48d9e63fab57c81d
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-02-25 12:32:34 +0000

    HW_7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+//HW_7
+
+//task #1
+const name = 'John';
+const admin = name;
+alert(admin);
+
+//task #2
+const planetName = 'Earth';
+const userName = 'Alice';
+// console.log(planetName)
+// console.log(userName)
+
+
+//task #3
+const variable = '10' + 10;
+console.log(typeof variable);
+console.log(variable);
