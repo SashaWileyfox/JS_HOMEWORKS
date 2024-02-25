e33390983d24d588b586f24927975fb02c9dd188
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-02-25 12:30:57 +0000

    cleare mine

--- js/script.js
+++ js/script.js
@@ -1,19 +1,1 @@
 'use strict';
-//HW_7
-
-//task #1
-const name = 'John';
-const admin = name;
-alert(admin);
-
-//task #2
-const planetName = 'Earth';
-const userName = 'Alice';
-// console.log(planetName)
-// console.log(userName)
-
-
-//task #3
-const variable = '10' + 10;
-console.log(typeof variable);
-console.log(variable);
