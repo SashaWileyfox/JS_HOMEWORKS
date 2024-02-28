89af69cd9b83f9e50c3b173b5559e37d8e82896c
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-02-28 20:34:38 +0000

    HW_6

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // task #1
-let str1= prompt('some text');
-let str2= prompt('some text');
-let str3= prompt('some text');
-alert(str1 + ' ' + str2 + ' ' + str3)
+// let str1= prompt('some text');
+// let str2= prompt('some text');
+// let str3= prompt('some text');
+// alert(str1 + ' ' + str2 + ' ' + str3)
 
 // task #2
 let number = 12345;
-let digits = number.toString().split('').map(Number)
+let digits = number.toString().split('').map(Number);
 console.log(digits.join(' '));
