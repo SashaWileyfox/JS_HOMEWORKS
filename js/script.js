f706153f83e3bc30ba1cfa449e96cc05126f43c4
Oleksandra Honcharenko <94483473+SashaWileyfox@users.noreply.github.com>
2024-02-29 19:33:55 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // task #1
-let str1 = prompt('some text');
-let str2 = prompt('some text');
-let str3 = prompt('some text');
+const str1 = prompt('some text');
+const str2 = prompt('some text');
+const str3 = prompt('some text');
 alert(str1 + ' ' + str2 + ' ' + str3)
 
 // task #2
-let number = 12345;
-let digits = number.toString().split('').map(Number);
+const number = 12345;
+const digits = number.toString().split('').map(Number);
 console.log(digits.join(' '));
