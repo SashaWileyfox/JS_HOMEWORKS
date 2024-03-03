c1697555d68ae2a99e462f222c9a052772c6b667
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-03 18:52:00 +0000

    HW_11

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+let operation = prompt('Please select the operation (add, sub, mult, div):');
+const num1 = Number(prompt('Please enter the first number'));
+const num2 = Number(prompt('Please enter the second number'));
+let symbols = new Map([
+    ['add', '+'],
+    ['div', '/'],
+    ['mult', '*'],
+    ['sub', '-']
+]);
+const symbol = symbols.get(operation);
+let result = null;
+
+if (isNaN(num1)) {
+    alert('Invalid first number. Please enter a valid number.');
+} else {
+    if (isNaN(num2)) {
+        alert('Invalid second number. Please enter a valid number.');
+    } else {
+
+        if (operation === 'add') {
+            result = num1 + num2;
+        } else if (operation === 'sub') {
+            result = num1 - num2;
+        } else if (operation === 'mult') {
+            result = num1 * num2;
+        } else if (operation === 'div') {
+            result = num1 / num2;
+        }
+    }
+
+}
+
+alert(`${num1} ${symbol} ${num2} = ${result}`);
+
+
