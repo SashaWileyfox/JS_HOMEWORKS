401a1a8e450883c501bdbd0cc84328f31f0521b2
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-03 21:13:42 +0000

    HW_12

--- js/script.js
+++ js/script.js
@@ -1,37 +1,24 @@
 'use strict';
 
-let operation = prompt('Please select the operation (add, sub, mult, div):');
-const num1 = Number(prompt('Please enter the first number'));
-const num2 = Number(prompt('Please enter the second number'));
-let symbols = new Map([
-    ['add', '+'],
-    ['div', '/'],
-    ['mult', '*'],
-    ['sub', '-']
-]);
-const symbol = symbols.get(operation);
-let result = null;
+//task #1
 
-if (isNaN(num1)) {
-    alert('Invalid first number. Please enter a valid number.');
-} else {
-    if (isNaN(num2)) {
-        alert('Invalid second number. Please enter a valid number.');
-    } else {
-
-        if (operation === 'add') {
-            result = num1 + num2;
-        } else if (operation === 'sub') {
-            result = num1 - num2;
-        } else if (operation === 'mult') {
-            result = num1 * num2;
-        } else if (operation === 'div') {
-            result = num1 / num2;
-        }
-    }
-
+const user = {
+    name: 'John',
+    surname: 'Smith'
 }
+user.name = 'Pete';
 
-alert(`${num1} ${symbol} ${num2} = ${result}`);
+delete user.name;
 
+console.log(user)
+
+//task #3
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+const sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log('Accrued: ' + sum)
 
