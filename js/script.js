1f4956f35ceab73db9de52c1cff1203ddfa6d83d
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-03 16:30:12 +0000

    HW_10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const num1 = Number(prompt('Please enter the first number'));
+const num2 = Number(prompt('Please enter the second number'));
+
+console.log(`result: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`result: ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`result: ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`result: ${num1} / ${num2} = ${num1 / num2}`);
