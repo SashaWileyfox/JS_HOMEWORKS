d008ea5defecbceb519123e81fc01462842cfcf1
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-05 13:55:25 +0000

    HW_16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const num1 = Number(prompt('Please enter the first digit:'));
+const num2 = Number(prompt('Please enter the second digit:'));
+const num3 = Number(prompt('Please enter the third digit:'));
+
+const sum = `${(num1 + num2 + num3) / 3}`;
+
+alert(`result: (${num1} + ${num2} + ${num3}) / 3 = ${sum}`);
