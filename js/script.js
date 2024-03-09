7ce9e39e2640ecf645c66f7849921fc12b9d5981
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-09 15:07:59 +0000

    HW_18

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const numOrStr = prompt('input number or string');
+
+switch (true) {
+    case numOrStr === null:
+        console.log('you canceled');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty string');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+}
