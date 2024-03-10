1d82bef5a7c396c56c602970aad66afd4cae1b33
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-10 10:33:35 +0000

    HW_19

--- js/script.js
+++ js/script.js
@@ -2,5 +2,37 @@
 
 const number = +prompt('please enter the number')
 
-switch (true){
+
+// Option #1
+switch (true) {
+    case isNaN(+number):
+        alert('ви ввели не корректне число');
+        break;
+    case number === 1:
+        alert(`${number} рік`);
+        break;
+    case number === 2:
+    case number === 3:
+    case number === 4:
+        alert(`${number} роки`);
+        break;
+    default:
+        alert(`${number} років`)
 }
+
+// Option #2
+/*if (!isNaN(number)) {
+    if (number === 1) {
+        alert(`${number} рік`);
+    } else if (number >= 2 && number <= 4) {
+        alert(`${number} роки`);
+    } else if (number >= 5) {
+        alert(`${number} років`);
+    } else if (number === 0) {
+        alert(`${number} років`);
+    } else {
+        alert('ви ввели не корректне число');
+    }
+} else {
+    alert('ви ввели не корректне число');
+}*/
