8af81a12d0724770c13da5becf39dfdc8f605b46
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-14 12:17:56 +0000

    HW_24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const task = '----------task #'
+
+//task #1
+console.log(`${task}1`)
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let a = 0; a < arr.length; a++) {
+    sum += arr[a];
+}
+console.log(sum)
+
+//task #2
+console.log(`${task}2`)
+
+let sum1 = 0;
+const arr1 = [1, 2, 3, 4, 5];
+for (let a = 0; a < arr1.length; a++){
+    sum1 += arr1[a] **2;
+}
+console.log(sum1)
