2750179f5979b32d65c5052ab2a1bba912804f83
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-14 12:48:52 +0000

    HW_25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const number = +prompt('Please enter the number');
+if (number <= 1) {
+    console.log(NaN)
+} else {
+    let div = 2;
+
+    while (number % div !== 0) {
+        div++
+    }
+    console.log(div)
+}
+
