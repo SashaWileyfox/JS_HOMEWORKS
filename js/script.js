52a5ad32bd63ffae25cf64c8bfaa8176855c4b52
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-16 16:48:17 +0000

    HW_29

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const padString = function (string, length, symbol = '*', right = true) {
+    if (typeof string !== 'string') return 'Error you need to enter string';
+    if (typeof length !== 'number' || length < 0) return 'Error you need to enter number';
+    if (typeof symbol !== 'string' || symbol.length !== 1) return 'Error you need to enter 1 symbol';
+    if (typeof right !== 'boolean') return 'Error you need to enter boolean value';
+
+    if (string.length > length) return string.substring(0, length);
+
+    while (string.length < length) {
+        right ? string = string + symbol : string = symbol + string;
+    }
+    return string;
+}
+console.log(padString('hello', 8));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
+
