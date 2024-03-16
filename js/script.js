368a1626642679db31536343d3e2411f571ed94e
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-16 14:54:02 +0000

    HW_27

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const generateKey = function (lenght, characters) {
+    let result = '';
+    for (let a = 0; a < lenght; a++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return result;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxsdvkjsdkdvmavyz0123456789';
+const key = generateKey(8, characters)
+console.log(`${key} \nlength: ${key.length}`);
