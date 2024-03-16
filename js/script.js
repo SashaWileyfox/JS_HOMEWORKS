65aafbca76cae2841f41dd3e5a65f1b32f3657f4
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-16 15:15:25 +0000

    HW_28

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const number = +prompt('Please enter the number');
+const degree = +prompt('Please enter the degree');
+
+const foo = function (number, degree = 1) {
+    if (!Number(number)) return 'Error';
+
+
+    let result = '';
+    result += `${number} ** ${degree} = ${number ** degree}`;
+
+    return result;
+}
+alert(foo(number, degree))
