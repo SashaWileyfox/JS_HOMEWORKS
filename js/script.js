ec96aaa452c55bc058c9f308c695348bbe0736ee
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-17 20:23:45 +0000

    HW_30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const foo = function (arr = []) {
+    const fooArr = [];
+
+    for (let a = 0; a < arr.length; a++) {
+        if (arr[a] < 0) continue;
+        if (arr[a] > 0) fooArr.push(arr[a]);
+    }
+
+    if (fooArr.length > 0) {
+        return fooArr.join(', ')
+    } else {
+        return 'Empty array'
+    }
+
+
+}
+console.log(foo(arr));
