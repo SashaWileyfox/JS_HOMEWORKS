e00faa9f6b87a26bdb2b28b55cc5f5c05fd0586b
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-25 20:12:20 +0000

    main

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const num1 = 5;
+const num2 = 3;
+
+const sum = add(num1, num2);
+document.write(`${sum}\n`);
+
+const difference = subtract(num1, num2);
+document.write(`${difference}\n`);
+
+const product = multiply(num1, num2);
+document.write(`${product}\n`);
