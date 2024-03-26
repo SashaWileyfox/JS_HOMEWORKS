25f9f68c4eef0e85a72200c2ee26f07be5439ca8
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-03-26 17:30:46 +0000

    HW_32

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 'use strict';
-const task = '----------task #'
+const task = '----------task #';
 
 //task #1
 console.log(`${task}1`);
-const arr = ['a', 'b', 'c', 'd']
-const str1 = arr.slice(0, 2).join(' + ');
-const str2 = arr.slice(2, 4).join(' + ');
+const arr = ['a', 'b', 'c', 'd'];
+const str1 = arr.slice(0, 2)
+  .join(' + ');
+const str2 = arr.slice(2, 4)
+  .join(' + ');
 console.log(`${str1} \n${str2}`);
 
 //task #2
@@ -17,35 +19,34 @@ const result = arr2[0] * arr2[1] + arr2[2] * arr2[3];
 })*/
 console.log(result);
 
-
 //task #3
 console.log(`${task}3`);
 const arr3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 let num4;
-arr3.forEach(function (subarrey) {
-    subarrey.forEach(function (el) {
-        if (el === 4) num4 = el;
-    })
-})
+arr3.forEach(function(subarrey) {
+  subarrey.forEach(function(el) {
+    if (el === 4) num4 = el;
+  });
+});
 console.log(num4);
 
 //task #4
 console.log(`${task}4`);
 const obj = {
-    js: ['jQuery', 'Angular'],
-    php: 'hello',
-    css: 'world'
+  js: ['jQuery', 'Angular'],
+  php: 'hello',
+  css: 'world'
 };
-const element = obj.js.find(function (el) {
-    return el === 'jQuery'
-})
+const element = obj.js.find(function(el) {
+  return el === 'jQuery';
+});
 console.log(element);
 
 //task #5
 console.log(`${task}5`);
 const arr4 = [];
 for (let i = 1; i <= 9; i++) {
-    arr4.push('x'.repeat(i));
+  arr4.push('x'.repeat(i));
 }
 console.log(arr4);
 
@@ -53,17 +54,18 @@ console.log(arr4);
 console.log(`${task}6`);
 const arr5 = [];
 for (let i = 1; i <= 9; i++) {
-    arr5.push(String(i).repeat(i))
+  arr5.push(String(i)
+    .repeat(i));
 }
 console.log(arr5);
 
 //task #7
 console.log(`${task}7`);
 const arr7 = [];
-const arrayFill = function (value, lenght) {
-    arr7.length = lenght;
-    return arr7.fill(value);
-}
+const arrayFill = function(value, lenght) {
+  arr7.length = lenght;
+  return arr7.fill(value);
+};
 /*const arrayFill = function (value, end) {
     const arr = [];
     for (let i = 0; i < end; i++){
@@ -75,4 +77,46 @@ console.log(arrayFill('7', 5));
 
 //task #8
 console.log(`${task}8`);
-const arr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr8 = [7, 2, 3, 4, 5, 6, 1, 8, 9];
+let sum = 0;
+let count = 0;
+for (let i = 0; i < arr8.length; i++) {
+  sum += arr8[i];
+  count++;
+  if (sum > 10) break;
+}
+console.log(`Кількість елементів, які потрібно додати з початку масиву, щоб отримати суму більше 10: ${count}`);
+
+//task #9
+console.log(`${task}9`);
+const numbers = [1, 2, 3, 4, 5, 7, 9, 11];
+for (let i = 0; i < numbers.length / 2; i++) {
+  let swap = numbers[numbers.length - 1 - i];
+  numbers[numbers.length - 1 - i] = numbers[i];
+  numbers[i] = swap;
+}
+console.log(numbers);
+
+//task #10
+console.log(`${task}10`);
+const arr10 = [[1, 2, 3], [4, 5], [6]];
+let sum2 = 0;
+for (let a = 0; a < arr10.length; a++) {
+  for (let b = 0; b < arr10[a].length; b++) {
+    sum2 += arr10[a][b];
+  }
+}
+console.log(sum2);
+
+//task #11
+console.log(`${task}11`);
+const arr11 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let sum3 = 0;
+for (let a = 0; a < arr11.length; a++) {
+  for (let b = 0; b < arr11[a].length; b++) {
+    for (let c = 0; c < arr11[b].length; c++) {
+      sum3 += arr11[a][b][c];
+    }
+  }
+}
+console.log(sum3);
