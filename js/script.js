f25c4b57d14dd2fd572168aaaf2a22b957499f81
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-04-15 14:12:11 +0100

    HW_52

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const task = '----------';
+
+console.log(`${task}factorial`);
+const getFactorial = ((number) => {
+  if (number === 1 || number === 0) return 1;
+  return number * getFactorial(number - 1);
+});
+console.log(getFactorial(3));
+
+console.log(`${task}degree`);
+const degree = ((num, deg) => {
+  if (num === 0) return 'error';
+  if (deg === 0) return 1;
+  return num * degree(num, deg - 1);
+});
+console.log(degree(2, 1));
+
+console.log(`${task}sum`);
+const sum = ((a, b) => {
+  if (a === 0) return b;
+  return sum(a - 1, b + 1);
+});
+console.log(sum(2, 2));
