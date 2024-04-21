73f295bdf384bacc9da61ea100f7cc12530fb503
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-04-21 19:58:28 +0100

    HW_56

--- js/script.js
+++ js/script.js
@@ -10,22 +10,16 @@ const getAgeOrName = function (prefix = 'age:') {
   return prefix + ' ' + this;
 };
 console.log(getAgeOrName.apply(user.age));
-console.log(getAgeOrName.apply(user2.name, ['name:']));
+console.log(getAgeOrName.apply(user.name, ['name:']));
 
-console.log('---------------myApply 1');
-
-const myApply = function (context = this, prefix = ['age:']) {
-  return prefix + ' ' + context;
+console.log('---------------myApply');
+const get = function () {
+  return {
+    apply(context = this, prefix = ['age:']) {
+      return prefix + ' ' + context;
+    },
+  };
 };
-console.log(myApply(user.age));
-console.log(myApply(user.name, ['name:']));
-
-console.log('---------------myApply 2');
-const myApply2 = function (func, context, [...args]) {
-  return func.call(context, [...args]);
-};
-const get = function (prefix) {
-  return prefix + ' ' + this;
-};
-console.log(myApply2(get, user.name, ['name:']));
-console.log(myApply2(get, user2.name, ['name:']));
+const getting = get();
+console.log(getting.apply(user2.age));
+console.log(getting.apply(user2.name, ['name:']));
