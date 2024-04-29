dca1cdbd3fd041a78d9502a888964f5a4dafe1b5
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-04-29 14:05:22 +0100

    HW_54

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
-const user = {
-  name: 'Sasha',
-  age: 23,
-};
-const user2 = {
-  name: 'Olha',
-  age: 29,
-};
-const getHelloName = function (hello, punctuation) {
-  return hello + ' ' + this + punctuation;
-};
-const fixValue = getHelloName.bind(user.name, 'Привіт');
-const fixValue2 = getHelloName.bind(user2.name, 'Привіт');
-
-console.log(fixValue('!'));
-console.log(fixValue2('.'));
-
-console.log('---------------myBind');
-
-const myBind = function (func, context, ...args) {
-  return function (...newArgs) {
-    return func.call(context, ...args, ...newArgs);
+(function () {
+  const user = {
+    name: 'Sasha',
+    age: 23,
   };
-};
 
-const split = function (hello, punctuation) {
-  return hello + ' ' + this + punctuation;
-};
-
-const getHelloNameMyBind = myBind(split, user.name, 'Привіт');
-const getHelloNameMyBind2 = myBind(split, user2.name, 'Привіт');
-
-console.log(getHelloNameMyBind('!'));
-console.log(getHelloNameMyBind2('.'));
+  
+})();
