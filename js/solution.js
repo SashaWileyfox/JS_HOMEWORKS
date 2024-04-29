2e76208ef6b6e3fd41b93d63fa0ca4cdd94a6c5d
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-04-29 14:41:56 +0100

    HW_54

---
+++ js/solution.js
@@ -0,0 +1,13 @@
+const bodyChild = document.body.firstElementChild;
+const list = bodyChild.children;
+let count = 0;
+const arr = [];
+for (const element of list) {
+  if (element instanceof HTMLElement) {
+    count += 1;
+    console.log(element);
+    arr.push(element.textContent);
+  }
+}
+console.log('amount of elements:', count);
+console.log(arr);
