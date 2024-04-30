10dfbdde717178d0512862ec5da1f6cd85eaebc0
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-04-30 19:37:23 +0100

    HW_54

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@
     name: 'Sasha',
     age: 23,
   };
+
+  const myCall = function (ctx = this, args = 'name: ') {
+    return args + ctx;
+  };
+  console.log(myCall(user.age, 'age: '));
+
+  const myBind = function (ctx = this, args = 'Hello, ') {
+    return function () {
+      return myCall(ctx, args);
+    };
+  };
+  console.log(myBind(user.name, 'Good morning, ')());
 })();
