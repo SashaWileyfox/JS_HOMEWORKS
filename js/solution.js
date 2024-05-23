d98d377320fd9fffdb377cb8d7da550375306afe
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-05-23 16:05:01 +0100

    HW_65

--- js/solution.js
+++ js/solution.js
@@ -1,16 +1,18 @@
 (function () {
-  const ul = document.createElement('ul');
-  const dataFromForm = JSON.parse(localStorage.getItem('formData'));
-  for (const key in dataFromForm) {
-    const li = document.createElement('li');
-    if (dataFromForm.hasOwnProperty(key)) {
-      if (dataFromForm[key] === '') {
-        li.textContent = `${key}: ERROR NO DATA`;
-      } else {
-        li.textContent = `${key}: ${dataFromForm[key]}`;
+  if (window.location.pathname.includes('list.html')) {
+    const ul = document.createElement('ul');
+    const dataFromForm = JSON.parse(localStorage.getItem('formData'));
+    for (const key in dataFromForm) {
+      const li = document.createElement('li');
+      if (dataFromForm.hasOwnProperty(key)) {
+        if (dataFromForm[key] === '') {
+          li.textContent = `${key}: ERROR NO DATA`;
+        } else {
+          li.textContent = `${key}: ${dataFromForm[key]}`;
+        }
       }
+      ul.append(li);
     }
-    ul.append(li);
+    document.body.append(ul);
   }
-  document.body.append(ul);
 })();
