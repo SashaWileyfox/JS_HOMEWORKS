76118fe287733bb2e3ee1254c08d3c00ce25abec
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-07-01 16:26:27 +0100

    hw_75

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
   const getComments = async (postId) => {
     try {
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+      if (!response.ok) throw new Error('Network response was not ok');
       const comments = await response.json();
       console.log('Коментарі:', comments);
     } catch (error) {
@@ -12,34 +13,40 @@
     }
   };
 
-  const getValue = () => {
+  const getPost = async (postId) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();
+      return data.find((item) => item.id === postId);
+    } catch (error) {
+      console.error('Помилка при отриманні поста:', error);
+    }
+  };
+
+  const getValue = async () => {
     try {
       const inputValue = document.getElementById('input-id').value;
       const postId = Number(inputValue);
 
-      if (!isNaN(postId) && postId <= 100 && postId > 0) {
-        const getPost = async () => {
-          const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-
-          const data = await response.json();
-          const post = data.find((item) => item.id === postId);
-          if (post) {
-            postContainer.innerHTML = `
-            <h2>${post.title}</h2>
-            <p>ID користувача: ${post.userId}</p>
-            <p>Завершено: ${post.completed ? 'Так' : 'Ні'}</p>  
-          `;
-            const commentButton = document.createElement('button');
-            commentButton.textContent = 'Отримати коментарі';
-            commentButton.addEventListener('click', () => getComments(post.id));
-            postContainer.appendChild(commentButton);
-          } else {
-            console.log('Пост з таким ідентифікатором не знайдено.');
-          }
-        };
-        getPost();
-      } else {
+      if (isNaN(postId) || postId <= 0 || postId > 100) {
         throw new Error('ID має бути числом від 1 до 100');
+      }
+
+      const post = await getPost(postId);
+
+      if (post) {
+        postContainer.innerHTML = `
+          <h2>${post.title}</h2>
+          <p>ID користувача: ${post.userId}</p>
+          <p>Завершено: ${post.completed ? 'Так' : 'Ні'}</p>
+        `;
+        const commentButton = document.createElement('button');
+        commentButton.textContent = 'Отримати коментарі';
+        commentButton.addEventListener('click', () => getComments(post.id));
+        postContainer.appendChild(commentButton);
+      } else {
+        throw new Error('Пост з таким ідентифікатором не знайдено.');
       }
     } catch (error) {
       postContainer.innerHTML = `<p>${error.message}</p>`;
