fe4f8ef674816efdb24e19767c513b294ee62ec7
Oleksandra Honcharenko <sachawileyfox@gmail.com>
2024-07-07 16:54:33 +0100

    hw_75

--- js/script.js
+++ js/script.js
@@ -35,9 +35,6 @@
     `;
     postContainer.classList.add('card');
   };
-
-  const renderComents = ()
-
   const handleError = (error) => {
     postContainer.innerHTML = `<p>${error.message}</p>`;
   };
